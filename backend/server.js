0472be76152254ca08a570e9449b334adb6a2c01
Alok Kumar <alokk13799@gmail.com>
2023-02-16 20:00:32 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+/** step1: express module required to create express server */
+const express = require("express");
+
+/** step2: execute the express() method which is create an web server
+ * and user can interact with web browser.
+ * web application is underlying part of web server which is process the user request come through web server and
+ * return the response to web server and webserver return response to user's browser.
+ */
+const app = express();
+
+const port = 8000;
+
+/** server home router */
+app.get("/", (req, res) => {
+	return res("Hello Express");
+});
+
+/** listen(port, callback) method take  */
+app.listen(port, console.log(`server is up and running on ${port}`));
